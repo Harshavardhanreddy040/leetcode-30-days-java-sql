d3fa7cd53f779dfa9192cac29db6fa5eca44f188
Harshavardhanreddy040 <harshavardhanreddys040@gmail.com>
2025-12-02 20:02:39 +0530

    Add solution for containsDuplicate problem

---
+++ day2/java/#217 Contains Duplicate.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> h = new HashSet<Integer>();
+        for(int x: nums){
+            if(h.contains(x)) return true;
+            h.add(x);
+        }
+        return false;
+    }
+}
