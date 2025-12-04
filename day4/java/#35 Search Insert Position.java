9cff59b87fff9094d9e2e84a20e454d449fac357
Harshavardhanreddy040 <harshavardhanreddys040@gmail.com>
2025-12-04 22:48:40 +0530

    Implement searchInsert method for position search

---
+++ day4/java/#35 Search Insert Position.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int size = nums.length;
+        for(int i=0; i<size; i++){
+            if(nums[i] == target)
+                return i;
+            if(nums[i] > target)
+                return i;
+        }
+        return size;  
+    }
+}
